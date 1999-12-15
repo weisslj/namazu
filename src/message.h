8725e32bb85d852e7d4c8253065c6a599d66a788
atoru Takabayashi <satoru@namazu.org>
1999-12-15 09:14:18 +0000

    * src/message.h (MSG_MIME_HEADER): Reindented to column 1.

--- src/message.h
+++ src/message.h
@@ -2,9 +2,9 @@
 #define _MESSAGE_H
 
 #if defined(_WIN32) || defined(__EMX__)
-    #define MSG_MIME_HEADER  "Content-type: text/html\n\n"
+#define MSG_MIME_HEADER  "Content-type: text/html\n\n"
 #else
-    #define MSG_MIME_HEADER  "Content-type: text/html\r\n\r\n"
+#define MSG_MIME_HEADER  "Content-type: text/html\r\n\r\n"
 #endif
 
 

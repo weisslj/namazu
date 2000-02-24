1c94f37e0642f9fc2bef15acb61c5c1b91b7039b
KOSEKI Yoshinori <kose@wizard.tamra.co.jp>
2000-02-24 03:29:01 +0000

    Fix, namazu-dir`s'.

--- lisp/namazu.el
+++ lisp/namazu.el
@@ -2,7 +2,7 @@
 ;;
 ;; Mule 上で Namazu を利用した検索を行うための elisp です。
 ;;
-;;  $Id: namazu.el,v 1.11 2000-02-16 08:58:09 shirai Exp $
+;;  $Id: namazu.el,v 1.12 2000-02-24 03:29:01 kose Exp $
 
 (defconst namazu-version "namazu.el 1.0.3")
 
@@ -525,23 +525,23 @@ e.g.
           ()
         (ding))))))
 
-(defun namazu-make-field-completion-alist (namazu-dir)
+(defun namazu-make-field-completion-alist (namazu-dirs)
   "make \'+files:\' completion alist."
-  (let* ((dir (if (null namazu-dir)
-		  (or namazu-default-dir
-		      (setq namazu-default-dir (namazu-get-default-index-dir)))
-                 (or (cdr (assoc namazu-dir namazu-dir-alist))
-                     namazu-dir)))
-         (fl (and dir
-		  (setq dir (expand-file-name dir))
-		  (file-exists-p dir)
-                  (directory-files dir)))
-         fields file)
-    (while (setq file (car fl))
-      (and (string-match "NMZ.field.\\([^.]+\\)\\'" file)
+  (let (dir flist fields fname
+	 (dirs (namazu-split-dir 
+	   (if namazu-dirs
+	       (or (cdr (assoc namazu-dirs namazu-dir-alist)) namazu-dirs)
+	     (or namazu-default-dir
+		 (setq namazu-default-dir (namazu-get-default-index-dir)))))))
+    (while (setq dir (car dirs))
+      (if (file-exists-p dir)
+	  (setq flist (append (directory-files dir))))
+      (setq dirs (cdr dirs)))
+    (while (setq fname (car flist))
+      (and (string-match "NMZ.field.\\([^.]+\\)\\'" fname)
            (setq fields (append (list (list (format "+%s:"
-              (substring file (match-beginning 1) (match-end 1))))) fields)))
-      (setq fl (cdr fl)))
+              (substring fname (match-beginning 1) (match-end 1))))) fields)))
+      (setq flist (cdr flist)))
     fields))
 
 (defun namazu-search-config-file ()

5c4cb2b96aeba5742b9f48f7dbbd4a39f4808a50
NOKUBI Takatsugu <knok@daionet.gr.jp>
2000-12-26 04:59:01 +0000

    * filter/powerpoint.pl: Changed to use pptHtml (a component 	of xlHtml). 	(Contributed by Teranishi)

--- filter/powerpoint.pl
+++ filter/powerpoint.pl
@@ -1,6 +1,6 @@
 #
 # -*- Perl -*-
-# $Id: powerpoint.pl,v 1.5 2000-05-16 08:24:12 knok Exp $
+# $Id: powerpoint.pl,v 1.6 2000-12-26 04:59:01 knok Exp $
 # Copyright (C) 2000 Ken-ichi Hirose, 
 #               2000 Namazu Project All rights reserved.
 #     This is free software with ABSOLUTELY NO WARRANTY.
@@ -28,17 +28,34 @@ use strict;
 use File::Copy;
 require 'util.pl';
 require 'gfilter.pl';
+require 'html.pl';
 
 my $pptconvpath  = undef;
+my $utfconvpath = undef;
 
 sub mediatype() {
     return ('application/powerpoint');
 }
 
 sub status() {
-    $pptconvpath = util::checkcmd('doccat');
-    return 'yes' if defined $pptconvpath;
-    return 'no'; 
+	$pptconvpath = util::checkcmd('pptHtml');
+#	return 'no' unless defined $pptconvpath
+	if (defined $pptconvpath) {
+		if (!util::islang("ja")) {
+			return 'yes';
+		} else {
+			$utfconvpath = util::checkcmd('lv');
+			if (defined $utfconvpath) {
+				return 'yes';
+			} else {
+				return 'no';
+			}
+		} 
+	} else {
+		$pptconvpath = util::checkcmd('doccat');
+		return 'yes' if defined $pptconvpath;
+		return 'no'; 
+	}
 }
 
 sub recursive() {
@@ -61,6 +78,86 @@ sub add_magic ($) {
 }
 
 sub filter ($$$$$) {
+    my ($orig_cfile, $cont, $weighted_str, $headings, $fields)
+      = @_;
+    my $err = undef;
+
+    if (util::checkcmd('pptHtml')) {
+    $err = filter_ppt($orig_cfile, $cont, $weighted_str, $headings, $fields);
+    } else {
+    $err = filter_doccat($orig_cfile, $cont, $weighted_str, $headings, $fields);
+    }
+    return $err;
+}
+
+sub filter_ppt ($$$$$) {
+    my ($orig_cfile, $cont, $weighted_str, $headings, $fields)
+      = @_;
+    my $cfile = defined $orig_cfile ? $$orig_cfile : '';
+
+    my $tmpfile  = util::tmpnam('NMZ.powerpoint');
+    my $tmpfile2 = util::tmpnam('NMZ.powerpoint2');
+
+
+    util::vprint("Processing ms-powerpoint file ... (using  '$pptconvpath')\n");
+
+    {
+	my $fh = util::efopen("> $tmpfile");
+	print $fh $$cont;
+    }
+
+#
+
+    # handle a Japanese PowerPoint ocument correctly.
+    system("$pptconvpath $tmpfile > $tmpfile2");
+
+    {
+	my $fh = util::efopen("< $tmpfile2");
+	$$cont = util::readfile($fh);
+    }
+
+    # Code conversion for Japanese document.
+    if (util::islang("ja")) {
+	my $encoding = "u8"; # UTF-8
+	# Pattern for pptHtml
+	if ($$cont =~ m!^<FONT SIZE=-1><I>Last Updated&nbsp;using Excel 5.0 or 95</I></FONT><br>$!m) 
+	{
+	    $encoding = "s"; # Shift_JIS
+	}
+	{
+	    my $fh = util::efopen("> $tmpfile");
+	    print $fh $$cont;
+	}
+	system("$utfconvpath -I$encoding -Oej $tmpfile > $tmpfile2");
+	{
+	    my $fh = util::efopen("< $tmpfile2");
+	    $$cont = util::readfile($fh);
+	}
+    } 
+
+    # Extract the author and exclude pptHtml's footer at once.
+    $$cont =~ s!^<FONT SIZE=-1><I>Spreadsheet's Author:&nbsp;(.*?)</I></FONT><br>.*!!ms;  # '
+    $fields->{'author'} = $1 if defined $1;
+
+    unlink($tmpfile);
+    unlink($tmpfile2);
+
+    # Title shoud be removed.
+    $$cont =~ s!<TITLE>.+</TITLE>!!;
+
+    html::html_filter($cont, $weighted_str, $fields, $headings);
+
+    gfilter::line_adjust_filter($cont);
+    gfilter::line_adjust_filter($weighted_str);
+    gfilter::white_space_adjust_filter($cont);
+
+    $fields->{'title'} = gfilter::filename_to_title($cfile, $weighted_str);
+    gfilter::show_filter_debug_info($cont, $weighted_str,
+			   $fields, $headings);
+    return undef;
+}
+
+sub filter_doccat ($$$$$) {
     my ($orig_cfile, $cont, $weighted_str, $headings, $fields)
       = @_;
     my $cfile = defined $orig_cfile ? $$orig_cfile : '';

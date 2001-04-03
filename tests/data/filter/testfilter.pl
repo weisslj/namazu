301c84f7df7d7d68156cc9e73970fc64cf162fa1
NOKUBI Takatsugu <knok@daionet.gr.jp>
2001-04-03 07:08:01 +0000

    Added some files for a test case.

---
+++ tests/data/filter/testfilter.pl
@@ -0,0 +1,79 @@
+#
+# -*- Perl -*-
+# $Id: testfilter.pl,v 1.1 2001-04-03 07:08:01 knok Exp $
+# Copyright (C) 2000 Namazu Project All rights reserved ,
+#     This is free software with ABSOLUTELY NO WARRANTY.
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either versions 2, or (at your option)
+#  any later version.
+# 
+#  This program is distributed in the hope that it will be useful
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+#  02111-1307, USA
+#
+#  This file must be encoded in EUC-JP encoding
+#
+
+package testfilter;
+use strict;
+require 'util.pl';
+
+my $dvipath = undef;
+my $nkfpath = undef;
+
+sub mediatype() {
+    return ('x-test/x-test');
+}
+
+sub status() {
+    return 'yes';
+}
+
+sub recursive() {
+    return 0;
+}
+
+sub pre_codeconv() {
+    return 0;
+}
+
+sub post_codeconv () {
+    return 0;
+}
+
+sub add_magic ($) {
+    $magic->addMagicEntry("0\tNamazu-Filter-Text:\tx-test/x-test");
+    return;
+}
+
+sub filter ($$$$$) {
+    my ($orig_cfile, $cont, $weighted_str, $headings, $fields)
+      = @_;
+    my $cfile = defined $orig_cfile ? $$orig_cfile : '';
+
+    util::vprint("Processing namazu-filter-test file ... \n");
+
+    test_filter($contref, $weighted_str, $fields);
+
+    return undef;
+}
+
+sub test_filter {
+    my ($contref, $weighted_str, $fields) = @_;
+
+    if ($$contref =~ s/^Namazu-Filter-Test: (.*)\n//m) {
+	$weighted_str .= "\x7f1\x7f$1\x7f/1\x7f\n" if defined $1;
+	my $title = $1 if defined $3;
+	$fields->{'title'} = $title;
+    }
+}
+
+1;

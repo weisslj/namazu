46ca33692ad5d3260cd5d4fd32109e2f222d3256
NOKUBI Takatsugu <knok@daionet.gr.jp>
2002-01-11 01:44:53 +0000

    Added a notice about customizing CGI_QUERY_MAX value. KF <dotslash at snosoft.com> was noticed me the probrem, thanks)

--- src/namazu.h
+++ src/namazu.h
@@ -7,6 +7,9 @@ enum {
     PAGE_MAX            = 20,  /* Max number of result pages */
     RESULT_MAX          = 100, /* Max number of result displays at once */
     CGI_QUERY_MAX       = 512, /* Max length of a CGI query */
+				/* If you want to customize the paramater, */
+				/* it MUST be smaller than BUFSIZE. */
+				/* BUFSIZE is defined in libnamazu.h */
     CGI_INDEX_NAME_MAX  = 64,  /* Max length of an index name in a CGI query */
     CGI_RESULT_NAME_MAX = 64,  /* Max length of a result name in a CGI query */
 

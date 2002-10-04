92829839f8156c450c3fc05a35be2a40ab50517e
Ryuji Abe <rug@namazu.org>
2002-10-04 06:25:10 +0000

    added polish translations. (contributed by Krzysztof Drewicz)

---
+++ template/NMZ.result.normal.pl
@@ -0,0 +1,5 @@
+<dt>${namazu::counter}. <strong><a href="${uri}">${title}</a></strong> (wynik: ${namazu::score})
+<dd><strong>Autor</strong>: <em>${author}</em>
+<dd><strong>Data</strong>: <em>${date}</em>
+<dd>${summary}
+<dd><a href="${uri}">${uri}</a> (${size} bajtów)<br><br>

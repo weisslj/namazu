853aed8043820bc14252bb8e8cf289b756bceef5
TADAMASA TERANISHI <yw3t-trns@asahi-net.or.jp>
2009-01-29 07:56:21 +0000

    * pl/{gettext.pl.in, var.pl.in }: Arrangement of text of source file. * pl/{builtinwakati.pl, indexer.pl, codeconv.pl}: ditto.

--- pl/codeconv.pl
+++ pl/codeconv.pl
@@ -1,15 +1,15 @@
 #
 # -*- Perl -*-
-# $Id: codeconv.pl,v 1.36 2008-07-20 07:14:58 opengl2772 Exp $
+# $Id: codeconv.pl,v 1.37 2009-01-29 07:56:22 opengl2772 Exp $
 # Copyright (C) 1997-1999 Satoru Takabayashi All rights reserved.
-# Copyright (C) 2000-2008 Namazu Project All rights reserved.
+# Copyright (C) 2000-2009 Namazu Project All rights reserved.
 #     This is free software with ABSOLUTELY NO WARRANTY.
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either versions 2, or (at your option)
 #  any later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -31,76 +31,76 @@ package codeconv;
 use strict;
 
 my @utf8_h2z = (
-	     0x82BF,
-	     0x8381, 0x8384, 0x8386, 0x8388, 0x838A, 0x838B, 0x838C, 0x838D,
-	     0x838E, 0x838F, 0x8392, 0x8395, 0x8398, 0x839B, 0x839E, 0x839F,
-	     0x83A0, 0x83A1, 0x83A2, 0x83A4, 0x83A6, 0x83A8, 0x83A9, 0x83AA,
-	     0x83AB, 0x83AC, 0x83AD, 0x83AF, 0x83B3, 0x829B, 0x829C,
-	     0x8080,  # $utf8_h2z[0x20] is not chosen. Filled a gap.
-	     0x8082, 0x808C, 0x808D, 0x8081, 0x83BB, 0x83B2, 0x82A1, 0x82A3,
-	     0x82A5, 0x82A7, 0x82A9, 0x83A3, 0x83A5, 0x83A7, 0x8383, 0x83BC,
-	     0x82A2, 0x82A4, 0x82A6, 0x82A8, 0x82AA, 0x82AB, 0x82AD, 0x82AF,
-	     0x82B1, 0x82B3, 0x82B5, 0x82B7, 0x82B9, 0x82BB, 0x82BD, );
+    0x82BF,
+    0x8381, 0x8384, 0x8386, 0x8388, 0x838A, 0x838B, 0x838C, 0x838D,
+    0x838E, 0x838F, 0x8392, 0x8395, 0x8398, 0x839B, 0x839E, 0x839F,
+    0x83A0, 0x83A1, 0x83A2, 0x83A4, 0x83A6, 0x83A8, 0x83A9, 0x83AA,
+    0x83AB, 0x83AC, 0x83AD, 0x83AF, 0x83B3, 0x829B, 0x829C,
+    0x8080,  # $utf8_h2z[0x20] is not chosen. Filled a gap.
+    0x8082, 0x808C, 0x808D, 0x8081, 0x83BB, 0x83B2, 0x82A1, 0x82A3,
+    0x82A5, 0x82A7, 0x82A9, 0x83A3, 0x83A5, 0x83A7, 0x8383, 0x83BC,
+    0x82A2, 0x82A4, 0x82A6, 0x82A8, 0x82AA, 0x82AB, 0x82AD, 0x82AF,
+    0x82B1, 0x82B3, 0x82B5, 0x82B7, 0x82B9, 0x82BB, 0x82BD, );
 
 # convert JIS X0201 KANA characters to JIS X0208 KANA
 sub utf8han2zen ($$) {
     my ($c1, $c2) = @_;
     if (util::islang("ja")) {
-	my $hi = "\xE3";
-	my $dakuten = "";
-	$c1 = unpack("n", $c1) & 0x7F;
-	my $mid_low = $utf8_h2z[$c1];
-	if ($c2) {
-	    if (unpack("n", $c2) == 0xBE9E) { # dakuten
-		if ($c1 == 0) {
-		    $mid_low = 0x8380; # "ta" to "da"
-		} elsif ($c1<=4 || ($c1>=0x0E && $c1<=0x0E) || $c1>=36) {
-		    $mid_low++;
-		} else {
-		    $dakuten = "\xE3\x82\x9B"; # isolated dakuten
-		}
-	    } elsif (unpack("n", $c2) == 0xBE9F) { # handakuten
-		if ($c1>=0x0A && $c1<=0x0E) {
-		     $mid_low += 2;
-		} else {
-		     $dakuten = "\xE3\x82\x9C"; # isolated handakuten
-		}
-	    }
-	}
+        my $hi = "\xE3";
+        my $dakuten = "";
+        $c1 = unpack("n", $c1) & 0x7F;
+        my $mid_low = $utf8_h2z[$c1];
+        if ($c2) {
+            if (unpack("n", $c2) == 0xBE9E) { # dakuten
+                if ($c1 == 0) {
+                    $mid_low = 0x8380; # "ta" to "da"
+                } elsif ($c1<=4 || ($c1>=0x0E && $c1<=0x0E) || $c1>=36) {
+                    $mid_low++;
+                } else {
+                    $dakuten = "\xE3\x82\x9B"; # isolated dakuten
+                }
+            } elsif (unpack("n", $c2) == 0xBE9F) { # handakuten
+                if ($c1>=0x0A && $c1<=0x0E) {
+                    $mid_low += 2;
+                } else {
+                    $dakuten = "\xE3\x82\x9C"; # isolated handakuten
+                }
+            }
+        }
         return $hi . pack("n",$mid_low) . $dakuten;
     }
 }
 
 sub utf8_han2zen_kana ($) {
     my ($strref) = @_;
-	$$strref =~ s/\xef(\xbd[\xa1-\xbf]|\xbe[\x80-\x9f])
-		     (\xef(\xbe[\x9e\x9f]))?
-		     /utf8han2zen($1,$3)/geox;
+    $$strref =~ s/\xef(\xbd[\xa1-\xbf]|\xbe[\x80-\x9f])
+        (\xef(\xbe[\x9e\x9f]))?
+        /utf8han2zen($1,$3)/geox;
 }
 
 # Remove a garbage utf-8 charactor at the end.
 sub chomp_multibytechar ($) {
     my ($str) = @_;
     {
-	# Remove a deficient utf-8 charactor at the end.
-	# ex. [C0-DF]$, [E0-EF][80-BF]$, [E0-EF]$ 
-	$str =~ s/[\xC0-\xDF]$//;
-	$str =~ s/[\xE0-\xEF][\x80-\xBF]?$//;
+        # Remove a deficient utf-8 charactor at the end.
+        # ex. [C0-DF]$, [E0-EF][80-BF]$, [E0-EF]$
+        $str =~ s/[\xC0-\xDF]$//;
+        $str =~ s/[\xE0-\xEF][\x80-\xBF]?$//;
     }
     return $str;
 }
 
 my %nkfopt_f  = ( '7BIT-JIS'    => 'J',    'ISO-2022-JP' => 'J',
                   'UTF-8'       => 'W',    'UTF8'        => 'W',
-                  'EUC-JP'      => 'E',  
-                  'SHIFT_JIS'   => 'S',    'SHIFT-JIS'   => 'S',  
+                  'EUC-JP'      => 'E',
+                  'SHIFT_JIS'   => 'S',    'SHIFT-JIS'   => 'S',
                   'SHIFTJIS'    => 'S',    'CP932'       => 'S',
                   'UTF16-BE'    => 'W16B', 'UTF16-LE'    => 'W16L',
                   'UNKNOWN'     => '' );
 my %nkfopt_t  = ( '7BIT-JIS'    => 'j',    'ISO-2022-JP' => 'j',
                   'UTF-8'       => 'w',    'UTF8'        => 'w',
-                  'EUC-JP'      => 'e',  
-                  'SHIFT_JIS'   => 's',    'SHIFT-JIS'   => 's',  
+                  'EUC-JP'      => 'e',
+                  'SHIFT_JIS'   => 's',    'SHIFT-JIS'   => 's',
                   'SHIFTJIS'    => 's',    'CP932'       => 's');
 
 sub from_to_by_nkf_m ($$$) {
@@ -210,7 +210,7 @@ sub encode_from_to ($$$) {
             if (ref $enc) {
                 $code_f = $enc->name;
                 util::dprint("Encode guessed : $code_f\n");
-            } elsif (!defined $enc){
+            } elsif (!defined $enc) {
                 $$contref = "";
                 util::dprint("Encode::Guess couldn't find coding name");
                 return "Encode::Guess couldn't find encoding";
@@ -221,13 +221,13 @@ sub encode_from_to ($$$) {
             }
         }
         Encode::from_to($$contref, $code_f ,$code_t);
-	util::dprint("Encode from $code_f to $code_t via encode_module");
+        util::dprint("Encode from $code_f to $code_t via encode_module");
     } else {
         # FIXME: nkf is a japanese limitation.
-        # In the future, code conversion functions will be replaced with 
+        # In the future, code conversion functions will be replaced with
         # Text::Iconv.
 
-        # FIXME: unnecessary since nkf 2.06. 
+        # FIXME: unnecessary since nkf 2.06.
         #
         # # nkf need UTF-16 BOM.
         # if ($code_f eq 'UTF-16BE') {
@@ -236,13 +236,13 @@ sub encode_from_to ($$$) {
         #     $$contref = "\xff\xfe" . $$contref;
         # }
 
-	if ($conf::NKF eq 'module_nkf') {
-	    from_to_by_nkf_m($contref, $code_f, $code_t);
-	    util::dprint("Encode from $code_f to $code_t via nkf_module");
-	} elsif ($conf::NKF ne 'no') {
-	    from_to_by_nkf($contref, $code_f, $code_t);
-	    util::dprint("Encode from $code_f to $code_t via nkf");
-	}
+        if ($conf::NKF eq 'module_nkf') {
+            from_to_by_nkf_m($contref, $code_f, $code_t);
+            util::dprint("Encode from $code_f to $code_t via nkf_module");
+        } elsif ($conf::NKF ne 'no') {
+            from_to_by_nkf($contref, $code_f, $code_t);
+            util::dprint("Encode from $code_f to $code_t via nkf");
+        }
     }
 }
 
@@ -267,9 +267,9 @@ sub to_inner_encoding ($$) {
     }
     if (util::islang("ja")) {
         $err = encode_from_to($contref, $code_from, $code_to);
-        if ($conf::NKF eq 'module_encode') { 
+        if ($conf::NKF eq 'module_encode') {
             decode_mime_header($contref, $code_to);
-	    normalize_jp($contref);
+            normalize_jp($contref);
         }
     }
     return $err;
@@ -280,7 +280,7 @@ sub to_external_encoding ($) {
 
     my $err;
     if ($util::EXT_ENCODE ne 'utf-8') {
-	$err = encode_from_to($contref, 'utf-8', $util::EXT_ENCODE);
+        $err = encode_from_to($contref, 'utf-8', $util::EXT_ENCODE);
     }
     return $err;
 }
@@ -293,12 +293,12 @@ sub load_encode {
         if ($@) {return $@};
         eval 'use Encode::Guess qw/ euc-jp shiftjis 7bit-jis utf-8 /;';
         if ($@) {return $@};
-	return undef;
+        return undef;
     } elsif (util::islang("ja") || util::islang_msg("ja")) {
-	if ($conf::NKF =~ /^module_nkf/) {
-	    eval 'use NKF 2.06;';
-	    if ($@) {return 'unable to require "NKF": ' . $@};
-	}
+        if ($conf::NKF =~ /^module_nkf/) {
+            eval 'use NKF 2.06;';
+            if ($@) {return 'unable to require "NKF": ' . $@};
+        }
     }
     return undef;
 }
@@ -306,23 +306,23 @@ sub load_encode {
 sub utf8_zen2han_ascii ($) {
     my ($strref) = @_;
     if (util::islang("ja")) {
-	# !#$%&()*+,./
-	# 0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_
-	$$strref =~ s/\xEF\xBC([\x81-\xBF])/chr(ord($1)-0x60)/ge;
-	# `abcdefghijklmnopqrstuvwxyz{|}
-	$$strref =~ s/\xEF\xBD([\x80-\x9D])/chr(ord($1)-0x20)/ge;
-	# space
-	$$strref =~ s/\xE3\x80\x80/ /g;
+        # !#$%&()*+,./
+        # 0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_
+        $$strref =~ s/\xEF\xBC([\x81-\xBF])/chr(ord($1)-0x60)/ge;
+        # `abcdefghijklmnopqrstuvwxyz{|}
+        $$strref =~ s/\xEF\xBD([\x80-\x9D])/chr(ord($1)-0x20)/ge;
+        # space
+        $$strref =~ s/\xE3\x80\x80/ /g;
     }
 }
 
 sub normalize_jp ($) {
     my ($contref) = @_;
     if (util::islang("ja")) {
-	{
+        {
             codeconv::utf8_han2zen_kana($contref);
             codeconv::utf8_zen2han_ascii($contref);
-	}
+        }
     }
     $contref;
 }

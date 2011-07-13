d6e7d4b8dd9c22f105ae855c1ef7aff2ae4f09e3
Yukio USUDA <usuda@hsba.go.jp>
2011-07-13 15:06:31 +0000

    * tests/namazu-cgi-12: Import from stable's pltests/namazu-cgi-12.pl. * tests/Makefile.am: Add new entry for namazu-cgi-12.

--- filter/man.pl
+++ filter/man.pl
@@ -1,9 +1,9 @@
 #
 # -*- Perl -*-
-# $Id: man.pl,v 1.38 2006-08-12 07:18:44 opengl2772 Exp $
+# $Id: man.pl,v 1.39 2011-07-13 15:06:34 usu Exp $
 # Copyright (C) 1997-2000 Satoru Takabayashi ,
 #               1999 NOKUBI Takatsugu ,
-#               2000-2005 Namazu Project All rights reserved.
+#               2004 Namazu Project All rights reserved.
 #     This is free software with ABSOLUTELY NO WARRANTY.
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -31,47 +31,15 @@ require 'gfilter.pl';
 
 my $roffpath = undef;
 my @roffopts = undef;
-my %env = ();
+my $envpath = undef;
+my @env = undef;
+my $exist_roff = undef;
 
 sub mediatype() {
     return ('text/x-roff');
 }
 
 sub status() {
-    $roffpath = util::checkcmd('jgroff');
-    $roffpath = util::checkcmd('groff') unless (defined $roffpath);
-    $roffpath = util::checkcmd('nroff') unless (defined $roffpath);
-    unless (defined $roffpath) {
-	return 'no';
-    }
-    %env = (
-        "LC_ALL" => $util::LANG,
-        "LANGUAGE" => $util::LANG,
-    );
-
-    if (util::islang("ja") && $roffpath =~ /\bj?groff$/) {
-	# Check wheter -Tnippon is valid.
-	my @cmd = ($roffpath, "-Tnippon", util::devnull());
-        my $status = util::syscmd(
-            command => \@cmd,
-            option => {
-                "stdout" => "/dev/null",
-                "stderr" => "/dev/null",
-            },
-            env => \%env,
-        );
-	if ($status == 0) {
-	    @roffopts = ('-man', '-Wall', '-Tnippon');
-	} else {
-	    @roffopts = ('-man', '-Wall', '-Tascii');
-	}
-    } elsif ($roffpath =~ /\bj?groff$/) {
-	@roffopts = ('-man', '-Tascii');
-    } elsif ($roffpath =~ /nroff$/) {
-	@roffopts = ('-man');
-    } else {
-	return 'no';
-    }
     return 'yes';
 }
 
@@ -96,50 +64,9 @@ sub filter ($$$$$) {
 	= @_;
     my $cfile = defined $orig_cfile ? $$orig_cfile : '';
 
-    util::vprint("Processing man file ... (using  '$roffpath @roffopts')\n");
-
-    my $tmpfile = util::tmpnam('NMZ.man');
-    {
-	my $fh = util::efopen("> $tmpfile");
-
-	# Make groff output one paragraph per one line.
-	# Thanks to Tatsuo SEKINE <tsekine@isoternet.org> for his suggestion.
-	print $fh ".ll 100i\n";
-
-	print $fh $$cont;
-        util::fclose($fh);
-    }
-    {
-	my @cmd = ($roffpath, @roffopts, $tmpfile);
-        my $fh_out = IO::File->new_tmpfile();
-        my $status = util::syscmd(
-            command => \@cmd,
-            option => {
-                "stdout" => $fh_out,
-                "stderr" => "/dev/null",
-            },
-            env => \%env,
-        );
-        my $size = util::filesize($fh_out);
-	if ($size == 0) {
-            util::fclose($fh_out);
-            unlink $tmpfile;
-	    return "Unable to convert file ($roffpath error occurred)";
-	}
-	if ($size > $conf::FILE_SIZE_MAX) {
-            util::fclose($fh_out);
-            unlink $tmpfile;
-	    return 'Too large man file';
-	}
-	$$cont = util::readfile($fh_out);
-        util::fclose($fh_out);
-    }
-    unlink $tmpfile;
-
-    codeconv::to_inner_encoding($cont, undef);
-
-    man_filter($cont, $weighted_str, $fields);
-
+    #codeconv::to_inner_encoding($cont, undef);
+    codeconv::toeuc($cont, undef);
+    roff_filter($cont, $weighted_str, $fields);
     gfilter::line_adjust_filter($cont);
     gfilter::line_adjust_filter($weighted_str);
     gfilter::white_space_adjust_filter($cont);
@@ -148,38 +75,46 @@ sub filter ($$$$$) {
     return undef;
 }
 
-# This is not perfect but works not bad.
-sub man_filter ($$$) {
+sub roff_filter ($$$) {
     my ($contref, $weighted_str, $fields) = @_;
-    my $name = "";
 
-    # remove escape sequence
-    $$contref =~ s/\x1b\[[01]m//gs;
-
-    # processing like col -b (2byte character acceptable)
-    $$contref =~ s/_\x08//g;
-    $$contref =~ s/\x08{1,2}([\x20-\x7e]|[\xa1-\xfe]{2})//g;
-
-    $$contref =~ s/^\s+//gs;
-
-    $$contref =~ /^(.*?)\s*\S*$/m;
-    my $title = "$1";
+    my $title = "";
+    my $comment = "";
+    my @lines;
+    @lines = split(/\n/, $$contref);
+    $$contref = "";
+    while (@lines){
+	my $str = shift @lines;
+	if ($str =~ /^\.TH ([^\s]*)(.*)$/) {
+	    $title = $1;
+	    $$contref .= $1 . " " . $2 ."\n";
+	}elsif ($str =~ /^\.S[HSMB] (.*)$/){
+	    if ($1 =~ /NAME|̾��|̾��/){
+		do {
+		    $str = shift @lines;
+		} while ($str =~ /^\./);
+		chomp $str;
+		my $weight = $conf::Weight{'html'}->{'h1'};
+		$$weighted_str .= "\x7f$weight\x7f$str\x7f/$weight\x7f\n";
+		$$contref .=  $str . "::\n";
+	    }
+	}elsif ($str =~ /^\.[RBI]+ (.*)$/){
+	    $$contref .=  $1;
+	}elsif ($str =~ /^\.\\" (.*)$/){
+	    $comment .=  $1 . "\n";
+	}elsif ($str =~ /^\.ig/){
+	    $str ="";
+	    do {
+		$str = shift @lines;
+		$comment .=  $str;
+	    } while ($str !~ /^\./);
+	}elsif ($str =~ /^\..?/){
+	}else {
+	    $$contref .=  $str;
+	}
+    }
     $fields->{'title'} = $title;
-    my $weight = $conf::Weight{'html'}->{'title'};
-    $$weighted_str .= "\x7f$weight\x7f$title\x7f/$weight\x7f\n";
-
-    if ($$contref =~ /^(?:NAME|名前|名称)\s*\n(.*?)\n\n/ms) {
-	$name = "$1::\n";
-	$weight = $conf::Weight{'html'}->{'h1'};
-	$$weighted_str .= "\x7f$weight\x7f$1\x7f/$weight\x7f\n";
-    }
-
-    if ($$contref =~ 
-	s/\A(.+^(?:DESCRIPTION 解説|DESCRIPTIONS?|SHELL GRAMMAR|INTRODUCTION|【概要】|解説|説明|機能説明|基本機能説明)\s*\n)//ims) 
-    {
-	$$contref = $name . $$contref;
-	$$weighted_str .= "\x7f1\x7f$1\x7f/1\x7f\n";
-    }
+    # $$contref .= $comment;
 }
 
 1;
